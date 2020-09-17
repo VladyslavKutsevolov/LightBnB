54e6f6a52505f6e138ef9edf704ebc81e50f0296
Vladyslav <kytsevolov@gmail.com>
2020-09-17 15:40:40 -0600

    add property query created

--- server/database.js
+++ server/database.js
@@ -134,9 +134,54 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+  } = property;
+
+  // set value to 0  if not received
+  cost_per_night ? cost_per_night : (cost_per_night = 0);
+  parking_spaces ? parking_spaces : (parking_spaces = 0);
+  number_of_bathrooms ? number_of_bathrooms : (number_of_bathrooms = 0);
+  number_of_bedrooms ? number_of_bedrooms : (number_of_bedrooms = 0);
+
+  return pool.query(
+    `INSERT INTO 
+      properties(owner_id, title, 
+          description, thumbnail_photo_url, cover_photo_url,
+          cost_per_night, street, city, province, post_code,
+          country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES
+      ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+      RETURNING *;`,
+    [
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms,
+    ]
+  );
 };
 exports.addProperty = addProperty;
