04b680c5da66b928bd1509f6bb3fcdbae6f14f12
Vladyslav <kytsevolov@gmail.com>
2020-09-17 14:02:58 -0600

    get all users reservations

--- server/database.js
+++ server/database.js
@@ -58,17 +58,20 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool.query(
-    `SELECT 
+  return pool
+    .query(
+      `SELECT 
       * 
     FROM reservations 
-      JOIN properties ON prperty_id = properties.id
+      JOIN properties ON property_id = properties.id
       WHERE guest_id = $1
       LIMIT $2;
      `,
-    [guest_id, limit]
-  );
+      [guest_id, limit]
+    )
+    .then((res) => res.rows);
 };
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
